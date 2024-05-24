a3bce00af7399426ec275726dda5d7dacc681743
Gustavosobrera <165527223+Gustavosobrera@users.noreply.github.com>
2024-05-24 11:36:04 -0300

    js aula01

---
+++ javascript/main.js
@@ -0,0 +1,43 @@
+//variavel é usada como algo que muda 
+console.log("ola gustavo")
+
+//texto -> string (guarda informaçoes)
+//numeros inteiros -> int (armazenas inteiros 1,2,3)
+//numeros decimais -> floot / double (floot guarda menos/double guarda mais informaçoes )
+//logicos -> boolean (representa o sim/não) em ingles seria (true or false)
+
+  //  var telefone = "19 99731-0108" // as ("") é significa que é uma string
+  //  var valormonetario = 50.59
+  //  var valorinteiro = 13 
+  //  var truefalse = true 
+
+//texto -> Guarde os dados do seu endereço em VARIAVEIS (ex. rua, bairro, cep, estado...)
+// Guarde os seus dados (nome, email, telefone, data de nascimento,genero) em VARIAVEIS
+//Crie uma VARIAVEL que diz que se voce é maior de idade
+//Crie uma VARIAVEL que diz se você pode dirigir
+
+
+var rua = "rua greçia,"
+var bairro = "jardim europa,"
+var cep = "13-459657,"
+var estado = "Sao Paulo."
+     console.log ("=======================================================") 
+console.log (rua, bairro, cep, estado)
+ 
+var  nome = "gustavo sobrera nunes dos santos"
+var  email = "gustavo.sobrera20@gmail.com"
+var  telefone = "31/01/2008"
+var  genero = "bmw"
+
+     console.log (nome)
+     console.log (email)
+     console.log (telefone)
+     console.log (genero) 
+     console.log ("=======================================================")  
+
+var  maior_de_idade = "false"
+ console.log (maior_de_idade)
+var  voce_pode_dirigir = "não"
+ console.log (voce_pode_dirigir)
+
+//texto->  fazer uma tabuada de 1 ate 10
